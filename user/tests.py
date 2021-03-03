9feeb86f1f9b03bb7b9fd26ba3b19662079d188b
castle_mac <castle_mac@castle-macui-MacBookPro.local>
2021-03-03 21:23:26 +0900

    Add: ProfileView 작성 완료, unittest 완료

--- user/tests.py
+++ user/tests.py
@@ -12,6 +12,7 @@ from unittest.mock  import (
 )
 
 from user.models import User
+from posting.models import JobCategory, Occupation
 from utils       import SECRET_KEY, ALGORITHM
 
 client = Client()
@@ -105,3 +106,50 @@ class SignUpTest(TestCase):
 
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json(), {'message': 'KEY_ERROR'})
+
+class ProfileTest(TestCase):
+    def setUp(self):
+        user = User.objects.create(
+            name  = '위코더',
+            email = 'wecode@test.com'
+        )
+        occu = Occupation.objects.create(name = '코딩')
+        job = JobCategory.objects.create(
+            name = '은우사랑모임',
+            occupation = occu
+        )
+        user.job_category.add(job)
+    
+    def tearDown(self):
+        user = User.objects.get(name='위코더')
+        user.job_category.all().delete
+        Occupation.objects.filter(name='코딩').delete
+        user.delete
+
+    def test_profile_patch_pass(self):
+        user  = User.objects.get(name='위코더')
+        token = jwt.encode({'id':user.id}, SECRET_KEY, algorithm=ALGORITHM)
+        data  = {'phoneNumber':'010-1234-1234'}
+
+        headers  = {'HTTP_AUTHORIZATION': token}
+        response = client.patch('/user/profile', json.dumps(data), content_type='applications/json', **headers)
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(),{'message': 'SUCCESS'})
+
+    def test_profile_patch_have_not_data(self):
+        user  = User.objects.get(name='위코더')
+        token = jwt.encode({'id':user.id}, SECRET_KEY, algorithm=ALGORITHM)
+
+        headers  = {'HTTP_AUTHORIZATION': token}
+        response = client.patch('/user/profile', content_type='applications/json', **headers)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(),{'message': 'JSON_DECODE_ERROR'})
+
+    def test_profile_get_have_not_token(self):
+        headers  = {'HTTP_AUTHORIZATION': ''}
+        response = client.patch('/user/profile', content_type='applications/json', **headers)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message': 'INVALID_TOKEN'})
