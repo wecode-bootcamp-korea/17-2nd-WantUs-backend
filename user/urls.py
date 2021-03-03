e7c59c41e1256bed6a63f5e6895efc903bd361b1
castle_mac <castle_mac@castle-macui-MacBookPro.local>
2021-03-03 21:23:26 +0900

    Add: ProfileView 작성 완료, unittest 완료

--- user/urls.py
+++ user/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from user.views import SignView
+from user.views import SignView, ProfileView
 
 urlpatterns = [
-    path('/sign', SignView.as_view())
+    path('/sign', SignView.as_view()),
+    path('/profile', ProfileView.as_view())
 ]
--- user/views.py
+++ user/views.py
@@ -1,12 +1,22 @@
 import json
 import jwt
 import requests
+from json.decoder   import JSONDecodeError
 
 from django.http  import JsonResponse
 from django.views import View
 
 from my_settings  import SECRET_KEY, ALGORITHM
-from user.models  import User
+from utils          import login_decorator
+from posting.models import (
+    Occupation,
+    JobCategory,
+)
+from user.models    import (
+    WorkExperience,
+    Skill,
+    User
+)
 
 class SignView(View):
     def get(self, request):
@@ -33,3 +43,33 @@ class SignView(View):
         
         except KeyError:
             return JsonResponse({'message':'KEY_ERROR'}, status = 400)
+            
+class ProfileView(View):
+    @login_decorator
+    def patch(self, request):
+        try:
+            data = json.loads(request.body)
+
+            user            = request.user
+            name            = data.get('name', user.name)
+            work_experience = data.get('workExperience', user.work_experience)
+            salary          = data.get('salary', user.salary)
+            phone_number    = data.get('phoneNumber', user.phone_number)
+
+            user.name            = name
+            user.work_experience = work_experience
+            user.salary          = salary
+            user.phone_number    = phone_number
+            user.save()
+
+            for skill in data.get('skills', []):
+                user.skill.add(skill)
+            
+            for job in data.get('jobCategory', []):
+                user.job_category.add(job)
+        
+        except JSONDecodeError:
+            return JsonResponse({'message':'JSON_DECODE_ERROR'}, status=400)
+        
+        return JsonResponse({'message':'SUCCESS'}, status=201)
+
