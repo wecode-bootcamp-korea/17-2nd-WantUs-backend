620fd7221e972fcb1d593abbaa9f877503f15771
castle_mac <castle_mac@castle-macui-MacBookPro.local>
2021-03-03 15:01:40 +0900

    Add: SignView 작성 완료, unittest 완료

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,107 @@
-from django.test import TestCase
+import json
+import bcrypt
+import jwt
 
-# Create your tests here.
+from django.test    import (
+    TestCase,
+    Client
+)
+from unittest.mock  import (
+    patch, 
+    MagicMock
+)
+
+from user.models import User
+from utils       import SECRET_KEY, ALGORITHM
+
+client = Client()
+
+class SignUpTest(TestCase):
+    def setUp(self):
+        bytes_pw  = bytes('12345678', 'utf-8')
+        hashed_pw = bcrypt.hashpw(bytes_pw, bcrypt.gensalt())
+
+        User.objects.create(
+            name      = '로그인',
+            email     = 'wecode_signin@naver.com',
+            image_url = 'https://wecode.co.kr/static/media/song.2cfd0cb6.png'
+        )
+
+    def tearDown(self):
+        User.objects.filter(name='로그인').delete()
+    
+    @patch('user.views.requests')
+    def test_signup_get_pass(self, mock_request):
+
+        class MockedResponse:
+            def json(self):
+                return {
+                    'id' : 1234,
+                    'kakao_account': {
+                        'profile' : {
+                            'nickname': '회원가입',
+                        },
+                        'email' : 'wecode_signup@naver.com',
+                    },
+                }
+            
+        mock_request.request = MagicMock(return_value = MockedResponse())
+        headers  = {'HTTP_AUTHORIZATION': '1234'}
+        response = client.get('/user/sign', content_type='applications/json', **headers)
+
+        user_id = jwt.decode(response.json()['accessToken'], SECRET_KEY, ALGORITHM)['id']
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {
+            'message': 'SUCCESS', 
+            'accessToken': jwt.encode({'id': user_id}, SECRET_KEY, algorithm=ALGORITHM)
+        })
+    
+    @patch('user.views.requests')
+    def test_signin_get_pass(self, mock_request):
+        
+        class MockedResponse:
+            def json(self):
+                return {
+                    'id' : 1234,
+                    'kakao_account': {
+                        'profile' : {
+                            'nickname': '로그인',
+                        },
+                        'email' : 'wecode_signin@naver.com',
+                    },
+                }
+            
+        mock_request.request = MagicMock(return_value = MockedResponse())
+        headers  = {'HTTP_AUTHORIZATION': '1234'}
+        response = client.get('/user/sign', content_type='applications/json', **headers)
+
+        user_id = jwt.decode(response.json()['accessToken'], SECRET_KEY, ALGORITHM)['id']
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(), {
+            'message': 'SUCCESS', 
+            'accessToken': jwt.encode({'id': user_id}, SECRET_KEY, algorithm=ALGORITHM)
+        })
+
+    @patch('user.views.requests')
+    def test_signup_get_key_error(self, mock_request):
+        
+        class MockedResponse:
+            def json(self):
+                return {
+                    'id' : 1234,
+                    'kakao_account': {
+                        'profile' : {
+                            'nickname': '로그인',
+                            'email' : 'wecode_signin@naver.com',
+                        },
+                    },
+                }
+            
+        mock_request.request = MagicMock(return_value = MockedResponse())
+        headers  = {'HTTP_AUTHORIZATION': '1234'}
+        response = client.get('/user/sign', content_type='applications/json', **headers)
+
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json(), {'message': 'KEY_ERROR'})
---
+++ user/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from user.views import SignView
+
+urlpatterns = [
+    path('/sign', SignView.as_view())
+]
