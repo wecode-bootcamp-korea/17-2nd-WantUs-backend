1dbfa99b2ddcb7ddaeed4966d09170bde35d25e4
강 현 수 <gangh84@gmail.com>
2021-03-05 14:39:09 +0900

    Modify: PostingDetailView 작성완료, Unittest 완료 및 Mock data 변경사항 반영
            리뷰 반영 및 프론트 소통 대기중

--- posting/views.py
+++ posting/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+import json
+import math
 
-# Create your views here.
+from django.http           import JsonResponse, HttpResponse
+from django.views          import View
+from django.core.paginator import Paginator
+
+from posting.models        import Posting, Like, BookMark
+from utils                 import non_user_accept_decorator
+
+class PostingDetailView(View):
+    @non_user_accept_decorator
+    def get(self, request, posting_id):
+        try:
+            user     = request.user
+            posting  = Posting.objects.get(id=posting_id)
+            result   = []
+            contents = {
+                    'id'           : posting.id,
+                    'title'        : posting.title,
+                    'company'      : posting.company_detail.company.name,
+                    'city'         : posting.company_detail.state.name,
+                    'district'     : posting.company_detail.county.name,
+                    'detailAddress': posting.company_detail.address,
+                    'latitude'     : float(posting.company_detail.latitude),
+                    'longitude'    : float(posting.company_detail.longitude),
+                    'tags'         : [tag_detail.name for tag_detail in posting.company_detail.company.tag.all()],
+                    'description'  : posting.description,
+                    'image'        : posting.company_detail.company.companyimage_set.first().image_url,
+                    'bonus'        : int(posting.reward),
+                    'like'         : posting.like_set.count(),
+                    'deadline'     : str(posting.end_date)[:10],
+                    'logoSrc'      : posting.company_detail.company.icon,
+                    'category'     : posting.job_category.occupation.name,
+                        }
+            if user is None:
+                contents['user']         = user
+                contents['userLike']     = False
+                contents['userBookmark'] = False
+                result.append(contents)
+                return JsonResponse({"message": "SUCCESS", "data": result}, status=200)
+
+            contents['user']         = user.name
+            contents['userLike']     = True if Like.objects.filter(user=user, posting=posting).exists() else False
+            contents['userBookmark'] = True if BookMark.objects.filter(user=user, posting=posting).exists() else False
+            result.append(contents)
+            return JsonResponse({"message": "SUCCESS", "data": result}, status=200)
+            
+        except Posting.DoesNotExist:
+            return JsonResponse({"message": "BAD_REQUEST"}, status=404) 
