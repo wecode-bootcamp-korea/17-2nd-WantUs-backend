ba05004cd9e4092d3b80a8b8d4a9e6f484a1d17b
castle_mac <castle_mac@castle-macui-MacBookPro.local>
2021-03-10 03:32:27 +0900

    Modify: apply models.py 수정

--- apply/models.py
+++ apply/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class Apply(models.Model):
     user           = models.ForeignKey('user.User', on_delete=models.CASCADE)
     posting        = models.ForeignKey('posting.Posting', on_delete=models.SET_NULL, null=True)
-    process_status = models.ForeignKey('ProcessStatus', on_delete=models.CASCADE)
-    reward_status  = models.ForeignKey('RewardStatus', on_delete=models.CASCADE)
+    process_status = models.ForeignKey('ProcessStatus', on_delete=models.CASCADE, default=1)
+    reward_status  = models.ForeignKey('RewardStatus', on_delete=models.CASCADE, default=1)
     update_at      = models.DateTimeField(auto_now=True)
     create_at      = models.DateTimeField(auto_now_add=True)
 
