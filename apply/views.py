8fe2dfb347c02e5c81435700d9b75c5e38b1e310
JELLO-Kim <cogus950403@gmail.com>
2021-03-10 03:45:17 +0900

    Add: MyWantUsView(GET) + unittest

--- apply/views.py
+++ apply/views.py
@@ -1,14 +1,30 @@
 import json
+from django.http        import JsonResponse
+from django.views       import View
+from django.db.models   import Count
 
-from django.views import View
-from django.http  import JsonResponse
-
-from apply.models  import Apply
-from resume.models import (
-    Resume,
-    ResumeFile
-)
-from utils         import login_decorator
+from .models        import (
+        Apply,
+        ProcessStatus
+        )
+from user.models    import User
+from utils          import login_decorator
+from resume.models  import Resume, ResumeFile
+from posting.models import (
+        Posting,
+        Occupation,
+        JobCategory,
+        CompanyImage,
+        CompanyDetail,
+        State,
+        County,
+        Tag,
+        TagDetail,
+        CompanyTag,
+        BookMark,
+        Like,
+        Company,
+        )
 
 class ApplyView(View):
     @login_decorator
@@ -58,3 +74,51 @@ class ApplyView(View):
         )
 
         return JsonResponse({'message':'SUCCESS'}, status=200)
+
+class MyWantUsView(View):
+    @login_decorator
+    def get(self, request):
+        user    = request.user
+        user_info = {
+                'profile'       : user.image_url,
+                'name'          : user.name,
+                'email'         : user.email,
+                'phoneNumber'   : user.phone_number
+                }
+        FIRST  = '지원 완료'
+        SECOND = '서류 통과'
+        THIRD  = '최종 합격'
+        FOURTH = '불합격'
+
+        apply_list = {
+                "stepOne"   : user.apply_set.filter(process_status__name = FIRST).count(),
+                "stepTwo"   : user.apply_set.filter(process_status__name = SECOND).count(),
+                "stepThree" : user.apply_set.filter(process_status__name = THIRD).count(),
+                "stepFour"  : user.apply_set.filter(process_status__name = FOURTH).count()
+                }
+
+        DATETIME_ONLY_TIME = 10
+        book_mark_posting = [{
+            'id'            : posting.id,
+            'category'      : posting.job_category.occupation.name,
+            'city'          : posting.company_detail.state.name,
+            'company'       : posting.company_detail.company.name,
+            'end'           : str(posting.end_date)[:DATETIME_ONLY_TIME],
+            'image'         : posting.company_detail.company.companyimage_set.first().image_url,
+            'state'         : posting.company_detail.county.name,
+            'subCategory'   : posting.job_category.name,
+            'tags'          : [TagDetail.objects.get(id=tag_detail_id_dict['tag_detail_id']).name
+                                for tag_detail_id_dict in posting.company_detail.company.companytag_set.values()],
+            'title'         : posting.title,
+            } for posting in Posting.objects.filter(bookmark__user = user)]
+        
+        like_posting = [{
+            'id'        : posting.id,
+            'city'      : posting.company_detail.state.name,
+            'company'   : posting.company_detail.company.name,
+            'image'     : posting.company_detail.company.companyimage_set.first().image_url,
+            'state'     : posting.company_detail.county.name,
+            'title'     : posting.title,
+            } for posting in Posting.objects.filter(like__user=user)]
+
+        return JsonResponse({'user' : user_info, "apply" : apply_list, "book" : book_mark_posting, "like" : like_posting}, status=200)
