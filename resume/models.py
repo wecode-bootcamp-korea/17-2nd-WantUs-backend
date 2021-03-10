1ebd9b8fe63cc4cfedc1985b602e4ffe6c52e809
castle_mac <castle_mac@castle-macui-MacBookPro.local>
2021-03-10 12:01:42 +0900

    Modify: models  수정

--- resume/models.py
+++ resume/models.py
@@ -6,7 +6,7 @@ from datetime import datetime
 class Resume(models.Model):
     user            = models.ForeignKey('user.User', on_delete=models.CASCADE)
     title           = models.CharField(max_length=50)
-    introduce       = models.CharField(max_length=300)
+    introduce       = models.CharField(max_length=300, null=True)
     is_default      = models.BooleanField(default=False)
     complete_status = models.ForeignKey('ResumeStatus', on_delete=models.CASCADE, default=1)
     create_at       = models.DateField(auto_now_add=True)
@@ -16,13 +16,14 @@ class Resume(models.Model):
         db_table = 'resumes'
 
 class ResumeFile(models.Model):
-    user       = models.ForeignKey('user.User', on_delete=models.CASCADE)
-    title      = models.CharField(max_length=50)
-    file_url   = models.URLField(max_length=2000)
-    is_default = models.BooleanField(default=False)
-    create_at  = models.DateField(auto_now_add=True)
-    update_at  = models.DateField(auto_now=True)
-    uuidcode   = models.CharField(max_length=40)
+    user            = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    title           = models.CharField(max_length=50)
+    file_url        = models.URLField(max_length=2000)
+    is_default      = models.BooleanField(default=False)
+    create_at       = models.DateField(auto_now_add=True)
+    update_at       = models.DateField(auto_now=True)
+    uuidcode        = models.CharField(max_length=40)
+    complete_status = models.ForeignKey('ResumeStatus', on_delete=models.CASCADE, default=3)
 
     class Meta:
         db_table = 'resume_files'
